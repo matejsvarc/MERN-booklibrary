689242215c762057f0b45e6a29b617c2cd8f070e
Matěj Švarc <mates.svarc@gmail.com>
2024-09-16 18:26:17 +0200

    improving UI

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
+app.use(cors());
+/*app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 })
 );
-
+*/
 app.get('/', (request, response) => {
     console.log(request); // Fix typo here
     return response.status(234).send('Hello World');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import EditBooks from './pages/EditBooks'
+import DeleteBook from './pages/DeleteBook'
+import ShowBook from './pages/ShowBook'
 
 export default function App() {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='books/create' element={<CreateBooks />} />
+      <Route path='books/details/:id' element={<ShowBook />} />
+      <Route path='books/edit/:id' element={<EditBooks />} />
+      <Route path='books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
