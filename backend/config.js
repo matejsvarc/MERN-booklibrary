eb3d33a7381ab504c481503b2c5900e928ed99b9
Matěj Švarc <71598789+matejsvarc@users.noreply.github.com>
2024-09-24 15:58:28 +0200

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://root:test@atlascluster.gpcizmt.mongodb.net/books-collection?retryWrites=true&w=majority&appName=AtlasCluster';
